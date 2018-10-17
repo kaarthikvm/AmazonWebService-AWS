ec407575f55bb6c1556dc7399cd7b154de8f76b8
kaarthik.vm <kaarthik.vm@gmail.com>
2018-10-18 00:13:27 +0100

    Create EC2-Create.py
    Creation of EC2 using python AWS SDK

---
+++ EC2-Create.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import boto3
+ec2 = boto3.resource('ec2')
+
+# create a file to store the key locally
+with open('ec2-keypair.pem','w') as outfile:
+
+    # call the boto ec2 function to create a key pair
+    key_pair = ec2.create_key_pair(KeyName='ec2-keypair')
+
+    # capture the key and store it in a file
+    KeyPairOut = str(key_pair.key_material)
+    print(KeyPairOut)
+    outfile.write(KeyPairOut)
+
+
+# create a new EC2 instance
+instances = ec2.create_instances(
+     ImageId='ami-0b59bfac6be064b78',
+     MinCount=1,
+     MaxCount=1,
+     InstanceType='t2.micro',
+     KeyName='ec2-keypair',
+
+ )
