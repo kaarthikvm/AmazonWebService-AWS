31f0bc08ed746db69ea8ebc09d9a37abf2bcee1f
kaarthik.vm <kaarthik.vm@gmail.com>
2018-10-18 01:06:52 +0100

    Update EC2-Create.py

--- EC2-Create.py
+++ EC2-Create.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import boto3
+import os
 ec2 = boto3.resource('ec2',region_name='us-east-2',
                            aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
                            aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
