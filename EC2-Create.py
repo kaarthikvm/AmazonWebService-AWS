f5c12ff72c243ff129ffc582930e7275d353dd8d
kaarthik.vm <kaarthik.vm@gmail.com>
2018-10-20 20:03:55 +0100

    Update EC2-Create.py

--- EC2-Create.py
+++ EC2-Create.py
@@ -2,27 +2,39 @@
 
 import boto3
 import os
-ec2 = boto3.resource('ec2',region_name='us-east-2')#,
+ec2 = boto3.client('ec2',region_name='us-east-2')#,
                            #aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
                            #aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
-# create a file to store the key locally
-with open('ec2-keypair.pem','w') as outfile:
 
-    # call the boto ec2 function to create a key pair
-    key_pair = ec2.create_key_pair(KeyName='ec2-keypair')
+def create_key_pair():
+    ''' create key pair '''
+    # create a file to store the key locally
+    with open('ec2-keypair.pem','w') as outfile:
 
-    # capture the key and store it in a file
-    KeyPairOut = str(key_pair.key_material)
-    print(KeyPairOut)
-    outfile.write(KeyPairOut)
+        # check whether key pair exits and delete it
+        response = ec2.delete_key_pair(KeyName='ec2-keypair',DryRun=False);
+        print response;
+
+
+        # call the boto ec2 function to create a key pair
+        key_pair = ec2.create_key_pair(KeyName='ec2-keypair')
+
+        # capture the key and store it in a file
+        KeyPairOut = str(key_pair['KeyMaterial'])
+        print(KeyPairOut)
+
+        outfile.write(KeyPairOut)
 
 
 # create a new EC2 instance
-instances = ec2.create_instances(
-     ImageId='ami-0b59bfac6be064b78',
-     MinCount=1,
-     MaxCount=1,
-     InstanceType='t2.micro',
-     KeyName='ec2-keypair',
+def create_instance():
+    instances = ec2.create_instances(
+                ImageId='ami-0b59bfac6be064b78',
+                MinCount=1,
+                MaxCount=1,
+                InstanceType='t2.micro',
+                KeyName='ec2-keypair')
 
- )
+if __name__ == "__main__":
+    create_key_pair();
+    create_instance();
