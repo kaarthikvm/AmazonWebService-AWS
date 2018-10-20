2df4ad68a3476156ed3ce232ad8de5a000f2c4fa
kaarthik.vm <kaarthik.vm@gmail.com>
2018-10-20 20:10:37 +0100

    Update EC2-Create.py

--- EC2-Create.py
+++ EC2-Create.py
@@ -1,10 +1,9 @@
-#!/usr/bin/python
-
 import boto3
 import os
-ec2 = boto3.client('ec2',region_name='us-east-2')#,
-                           #aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-                           #aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
+ec2Client = boto3.client('ec2')# Set these variables for JENKINS user manually as secret key cannot be used in public forum
+                           # region_name='us-east-2',
+                           # aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+                           # aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
 
 def create_key_pair():
     ''' create key pair '''
@@ -12,12 +11,12 @@ def create_key_pair():
     with open('ec2-keypair.pem','w') as outfile:
 
         # check whether key pair exits and delete it
-        response = ec2.delete_key_pair(KeyName='ec2-keypair',DryRun=False);
+        response = ec2Client.delete_key_pair(KeyName='ec2-keypair',DryRun=False);
         print response;
 
 
         # call the boto ec2 function to create a key pair
-        key_pair = ec2.create_key_pair(KeyName='ec2-keypair')
+        key_pair = ec2Client.create_key_pair(KeyName='ec2-keypair')
 
         # capture the key and store it in a file
         KeyPairOut = str(key_pair['KeyMaterial'])
@@ -27,8 +26,9 @@ def create_key_pair():
 
 
 # create a new EC2 instance
+ec2Resource=boto3.resource('ec2');
 def create_instance():
-    instances = ec2.create_instances(
+    instances = ec2Resource.create_instances(
                 ImageId='ami-0b59bfac6be064b78',
                 MinCount=1,
                 MaxCount=1,
